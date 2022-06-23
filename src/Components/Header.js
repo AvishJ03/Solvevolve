78404895c55a8c7ff6463d541caf5f241cd0044c
Avish Jain <80064831+AvishJ03@users.noreply.github.com>
2022-06-23 17:22:00 +0530

    bugs

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,12 +3,13 @@ export default function Header() {
     <div className="flex justify-between align-center mx-10 my-6">
       <h1 className="font-bold text-3xl ">Timeline</h1>
       <div className="flex justify-evenly align-center w-2/5">
-        <select id="drop" className="border-2 px-6">
+        <select id="drop" className="border-2 rounded-xl px-5">
           <option value="week">Week</option>
           <option value="month">Month</option>
           <option value="day">Day</option>
           <option value="year">Year</option>
         </select>
+        {/* e5e7eb */}
         <p className="text-lg mt-2 mx-4">19 - 25 June, 2022</p>
         <button className="border-2 px-3 py-2"> Prev </button>
         <button className="border-2 px-3 py-2 -ml-4"> Next </button>
